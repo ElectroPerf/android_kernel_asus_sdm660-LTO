787dd565c4fe3673b914a1cf41ee09200bc73e9b
Dede Dindin Qudsy <xtrymind@gmail.com>
2019-11-19 04:20:53 +0700

    msm: mdss: remove nvt_tp_check
    this kernel will only be used in txd version, so don't bother to check
    other version.
    
    Signed-off-by: Dede Dindin Qudsy <xtrymind@gmail.com>
    Change-Id: Ia725c791e00bc5a263b46b4b5e35dddd1d21f666

--- drivers/video/fbdev/msm/mdss_dsi.h
+++ drivers/video/fbdev/msm/mdss_dsi.h
@@ -24,10 +24,6 @@
 #include "mdss_dsi_cmd.h"
 #include "mdss_dsi_clk.h"
 
-#ifdef CONFIG_MACH_ASUS_X00TD
-extern int nvt_tp_check;
-#endif
-
 #define MMSS_SERDES_BASE_PHY 0x04f01000 /* mmss (De)Serializer CFG */
 
 #define MIPI_OUTP(addr, data) writel_relaxed((data), (addr))
